1a2eaad1a46aefadeaea5ba0497ae64fc9397a5d
davin0130 <davin0130@naver.com>
2023-04-07 23:46:01 +0900

    Modify:CodingTestFile:Study on Programmers lv.2 70% 1 prob

--- Programmers/lv2_training_(70%).py
+++ Programmers/lv2_training_(70%).py
@@ -6,25 +6,39 @@
 # print(nlist)
 
 
-#jadencase 문자열
+# #jadencase 문자열
 
-s = "for   THE  "
-slist = s.split(' ')
-print(slist)
-print(len(s))
-sstr = ""
+# s = "for   THE  "
+# slist = s.split(' ')
+# print(slist)
+# print(len(s))
+# sstr = ""
 
-for i in range(len(slist)):
-    slist[i] = slist[i].lower()
-    if slist[i] != "":
-        if slist[i][0].isdigit() == False:
-            slist[i] = slist[i].replace(slist[i][0], slist[i][0].upper())
+# for i in range(len(slist)):
+#     slist[i] = slist[i].lower()
+#     if slist[i] != "":
+#         if slist[i][0].isdigit() == False:
+#             slist[i] = slist[i].replace(slist[i][0], slist[i][0].upper())
 
-for i in range(len(slist)):
+# for i in range(len(slist)):
 
-    if i+1 == len(slist):
-        sstr += slist[i]
-    else:
-        sstr += slist[i] + " "
-print(sstr)
-print(len(sstr))
+#     if i+1 == len(slist):
+#         sstr += slist[i]
+#     else:
+#         sstr += slist[i] + " "
+# print(sstr)
+# print(len(sstr))
+
+
+# 최솟값 만들기
+A = [1,2]
+B = [3,4]
+answer = 0
+
+A = sorted(A)
+B = sorted(B, reverse=True)
+    
+for i in range(len(A)):
+    answer += A[i] * B[i]
+
+print(answer)
