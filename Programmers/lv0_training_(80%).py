9e7b96fa34dbf129760d002eb6608f06b58efee4
davin0130 <davin0130@naver.com>
2023-04-11 17:09:21 +0900

    Update lv0_training_(80%).py
    Modify:CodingTestFile:Study on Programmers | lv.0 12 probs

--- Programmers/lv0_training_(80%).py
+++ Programmers/lv0_training_(80%).py
@@ -84,14 +84,256 @@
 # print(answer)
 
 
-# 숫자 찾기
-num = 123456
-k = 7
+# # 숫자 찾기
+# num = 123456
+# k = 7
 
-num = str(num)
-k = str(k)
+# num = str(num)
+# k = str(k)
 
-if k in num:
-    print(str(num).index(str(k))+1)
-else:
-    print(-1)
+# if k in num:
+#     print(str(num).index(str(k))+1)
+# else:
+#     print(-1)
+
+
+
+# # 문자열 정렬하기 
+# my_string = "heLLo"
+# my_string = sorted([i for i in my_string.lower()])
+# answer = ''
+
+# for i in my_string:
+#     answer += i
+
+# print(answer)
+
+# # 간단한 방법
+# print(''.join(sorted(my_string.lower())))
+
+
+
+# # 합성수(약수가 3개 이상인 수) 찾기 
+# n = 15
+# answer = 0
+
+# for i in range(1, n+1):
+#     count = 0
+#     for j in range(1, i+1):
+#         if i%j == 0:
+#             count += 1
+#     if count >= 3:
+#         answer += 1
+
+# print(answer)
+
+
+# # 중복된 문자 제거
+# my_string = "We are the world"
+# answer = ""
+
+# for i in my_string:
+#     if i not in answer:
+#         answer += i
+#         print(answer)
+
+# # 간단한 방법
+# print(''.join(dict.fromkeys(my_string)))
+
+
+
+# # 모스부호(1)
+# morse = { 
+#     '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+#     '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+#     '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+#     '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+#     '-.--':'y','--..':'z'
+# }
+
+# letter = ".... . .-.. .-.. ---"
+# letter = letter.split(' ')
+# letter = [morse[i] for i in letter]
+# print(''.join(letter))
+
+
+
+# # A로 B 만들기
+# before = "hello"
+# after = "olleh"
+# answer = 1
+
+# before = [i for i in before]
+# after = [i for i in after]
+# for i in range(len(before)):
+#     if before[i] in after:
+#         after.pop(after.index(before[i]))
+#         print(before, after)
+#     else:
+#         answer = 0
+#         break
+
+# print(answer)
+
+
+# # 간단한 방법
+# # sort 후 똑같은지 비교.. 
+# if sorted(before) == sorted(after):
+#     answer = 1
+# else:
+#     answer = 0
+# print(answer)
+
+
+
+# # 팩토리얼  >>> fail
+# n = 3628800
+# count = 0
+# answer = 1
+# while answer <= n:
+#     for i in range(1, count+1):
+#         answer *= i
+#         print(i, answer)
+#     count += 1
+#     print(answer, count)
+#     print('---')
+
+
+# # 2차원으로 만들기
+# num_list = [100, 95, 2, 4, 5, 6, 18, 33, 948]
+# n = 3
+# answer = []
+
+# for i in range(int(len(num_list)/n)):
+#     answer.append(num_list[i*n:i*n+n])
+
+# print(answer)
+
+
+
+# # k의 개수
+# i = 1
+# j = 13
+# k = 1
+# answer = 0
+
+
+# for x in range(i, j+1):
+#     if str(k) in str(x):
+#         answer += str(x).count(str(k))
+        
+# print(answer)
+
+# # 간단한 방법
+# print(sum([str(i).count(str(k)) for i in range(i,j+1)]))
+
+
+
+# # 가까운 수
+# array = [11, 5, 28]
+# n = 8
+# answer = []
+
+# array = sorted(array)
+
+# for i in array:
+#     answer.append(abs(i-n))
+
+# print(answer)
+# print(array[answer.index(min(answer))])
+
+
+
+# # 진료 순서 정하기 
+# emergency = [30, 10, 23, 6, 100]
+# answer = []
+# em_dict = {}
+
+# emergency_sort = sorted(emergency, reverse=True)
+# for i in range(len(emergency)):
+#     em_dict[emergency_sort[i]] = i+1
+
+
+# for i in range(len(emergency)):
+#     answer.append(em_dict[emergency[i]])
+
+# print(answer)
+
+
+# # 숨어있는 숫자의 덧셈 (2)
+# import re
+# my_string = "aAb1B2cC34oOp"
+# answer = []
+
+# numbers = re.sub(r'[^0-9]', ' ', my_string).split(' ')
+# for i in numbers:
+#     if i != '':
+#         answer.append(int(i))
+
+# print(sum(answer))
+
+
+# # 간단한 방법
+# # my_string에서 숫자가 아닌 부분은 모두 공백으로 변환
+# s = ''.join(i if i.isdigit() else ' ' for i in my_string)
+# # 숫자인 부분이 문자열로 되어 있으니, integer로 변경 후 합계 구하기
+# print(sum(int(i) for i in s.split()))
+
+
+
+# # 한번만 등장한 문자
+# s = "abdc"
+# answer = ''
+# slist = list(set([i for i in s]))
+
+# for i in slist:
+#     if s.count(i) == 1:
+#         answer += i
+# print(''.join(sorted(answer)))
+
+# # 간단한 방법
+# print(''.join(sorted([i for i in set(s) if s.count(i) == 1])))
+
+
+
+# # 이진수 더하기
+# bin1 = "10"
+# bin2 = "11"
+
+# print(format(int(bin1, 2) + int(bin2, 2), 'b'))
+
+
+# # 소인수 분해
+# n = 420
+# count = 2
+# answer = []
+
+# while n != 1:
+#     if n % count != 0:
+#         count += 1
+#     elif n == 1:
+#         break
+#     else:
+#         answer.append(count)
+#         n = int(n / count)
+    
+#     print(n, count)
+# print(sorted(list(set(answer))))
+
+
+
+# # 컨트롤 제트 >>> fail(테스트케이스 1개)
+# s = "-1 -2 -3 Z"
+# answer = 0
+# s = s.split(' ')
+# print(s)
+
+# for i in range(len(s)):
+#     print(i, s[i])
+#     if str(abs(int(s[i]))).isdigit() == True:
+#         answer += int(s[i])
+#     else:
+#         print(s[i-1])
+#         answer -= int(s[i-1])
+
+# print(answer)
